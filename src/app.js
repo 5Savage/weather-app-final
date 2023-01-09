a420471e1aece911359f474ab4c25b2f2faa8aa1
Myah Savage <minniesavage5@gmail.com>
2023-01-09 10:14:05 -0500

    added weather icons from the api

--- src/app.js
+++ src/app.js
@@ -19,23 +19,30 @@ function formatDate(timestamp){
 
 
 function displayTemperature(response){
- 
+
     let temperatureElement=document.querySelector("#temperature");
     let cityElement=document.querySelector("#city");
     let descriptionElement=document.querySelector("#description");
     let humidityElement=document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
     let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
+
     temperatureElement.innerHTML= Math.round(response.data.temperature.current);
     cityElement.innerHTML=response.data.city;
     descriptionElement.innerHTML=response.data.condition.description;
     humidityElement.innerHTML=response.data.temperature.humidity;
     windElement.innerHTML=Math.round(response.data.wind.speed);
     dateElement.innerHTML= formatDate(response.data.time*1000);
+    iconElement.setAttribute(`src`,
+response.data.condition.icon_url
+);
+    iconElement.setAttribute(`alt`, response.data.condition.description);
 }
 
 let apiKey="1e1dc2b07543227bfo243f03dtab9c5f";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`
+let city= "New York";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
 
 axios.get(apiUrl).then(displayTemperature);
 
