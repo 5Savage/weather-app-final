876d5fcfbb4cb3107f2ea476fa0dc125076b8587
Myah Savage <minniesavage5@gmail.com>
2023-01-10 11:12:17 -0500

    created a new function to create a loop that will over ride the forecast HTML

--- src/app.js
+++ src/app.js
@@ -16,6 +16,32 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+    let forecastElement=document.querySelector("#forecast");
+    let forecastHTML= `<div class="row">`;
+    let days= ["Tue","Wed","Thur","Fri","Sat","Sun"];
+    days.forEach(function(day){
+         forecastHTML=
+    forecastHTML+ 
+         ` 
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  width="40"
+                />
+                <div class="weather-forecast-temp">
+                  <span class="weather-forecast-temp-max"> 18° </span>
+                  <span class="weather-forecast-temp-min"> 12° </span>
+                </div>
+              </div>`;
+
+    });
+            forecastHTML=forecastHTML+ `</div>`;
+            forecastElement.innerHTML=forecastHTML;
+         
+}
+
 
 
 function displayTemperature(response){
@@ -80,3 +106,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 
 search("New York");
+displayForecast();
